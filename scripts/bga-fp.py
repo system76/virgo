a5430adda299a22def4a115f62aa8f139124efce
Jeremy Soller <jackpot51@gmail.com>
2023-08-03 11:40:35 -0600

    Add MEC1521 pinout, update script to generate BGA automatically

--- scripts/bga-fp.py
+++ scripts/bga-fp.py
@@ -36,13 +36,26 @@ o.write("  (fp_text value " + module + " (at 0 1.27) (layer F.Fab)\n")
 o.write("    (effects (font (size 1 1) (thickness 0.15)))\n")
 o.write("  )\n")
 
-
+spacing = None
 for name, rows in units.items():
     for row in rows:
         if len(row) >= 4 and len(row[0]) >= 1:
             pin = row[0]
             x = float(row[3])/1000.0
             y = -float(row[4])/1000.0
+            o.write("  (pad " + pin + " smd circle (at " + str(x) + " " + str(y) + ") (size 0.25 0.25) (layers F.Cu F.Paste F.Mask))\n")
+        elif len(row) >= 2 and row[0] == "SPACING":
+            spacing = float(row[1])
+        elif spacing is not None and len(row) >= 2 and len(row[0]) >= 2:
+            pin = row[0]
+            # Skip I - is this always done?
+            if ord(pin[0]) >= ord('J'):
+                row = ord(pin[0]) - ord('J') + 8
+            else:
+                row = ord(pin[0]) - ord('A')
+            col = int(pin[1:]) - 1
+            x = float(col) * spacing
+            y = float(row) * spacing
             o.write("  (pad " + pin + " smd circle (at " + str(x) + " " + str(y) + ") (size 0.25 0.25) (layers F.Cu F.Paste F.Mask))\n")
 
 o.write(")\n")
